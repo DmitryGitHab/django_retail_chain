196307f6ef5b59ed08e84dada4da17756ed8d62d
Dmitry <babanov91@gmail.com>
2022-09-26 20:41:50 +0500

    add models.py v.0.2

--- api_shop/models.py
+++ api_shop/models.py
@@ -1,9 +1,10 @@
-from django_rest_passwordreset.tokens import get_token_generator
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
+from django_rest_passwordreset.tokens import get_token_generator
 
 
 
@@ -216,3 +217,49 @@ class ProductParameter(models.Model):
 
     def __str__(self):
         return f'{self.product} - {self.parameter} {self.value}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', related_name='orders', blank=True,
+                             on_delete=models.CASCADE)
+    status = models.CharField(verbose_name='Статус', choices=STATE_CHOICES, max_length=15, default='basket')
+    contact = models.ForeignKey(Contact, verbose_name='Контакт', blank=True, null=True,
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = "Список заказов"
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.created)
+        # return self.created
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, verbose_name='Заказ', related_name='ordered_items', blank=True,
+                              on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='Категория товара', blank=True, null=True,
+                                 on_delete=models.SET_NULL)
+    shop = models.ForeignKey(Shop, verbose_name='магазин', blank=True, null=True, on_delete=models.SET_NULL)
+    product_name = models.CharField(max_length=80, verbose_name='Название товара')
+    external_id = models.PositiveIntegerField(verbose_name='Внешний ИД')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
+    price = models.PositiveIntegerField(default=0, verbose_name='Цена')
+    total_amount = models.PositiveIntegerField(default=0, verbose_name='Общая стоимость')
+
+    class Meta:
+        verbose_name = 'Заказанная позиция'
+        verbose_name_plural = "Список заказанных позиций"
+        constraints = [
+            models.UniqueConstraint(fields=['order_id', 'product_name'], name='unique_order_item'),
+        ]
+
+    def __str__(self):
+        return self.product_name
+
+    def save(self, *args, **kwargs):
+        self.total_amount = self.price * self.quantity
+        super(OrderItem, self).save(*args, **kwargs)
